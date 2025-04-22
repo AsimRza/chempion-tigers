36897226b4264a3d964c6b65124df95a5ea07a77
“AsimRza” <asimrzali9@gmail.com>
2025-04-22 21:08:14 +0400

    task: add responsive navbar and banner section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let navbar = document.querySelector("header nav");
+  const navbar = document.querySelector("header nav");
+
+  const menuBtn = document.querySelector(".nav-menu-btn");
+
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  const mobileMenuCloseBtn = document.querySelector(".mobile-menu-close");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 200) {
@@ -7,5 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       navbar.classList.remove("scrolling-navbar");
     }
+  });
+
+  menuBtn.addEventListener("click", function () {
+    mobileMenu.classList.add("show");
+  });
+
+  mobileMenuCloseBtn.addEventListener("click", function () {
+    mobileMenu.classList.remove("show");
   });
 });
